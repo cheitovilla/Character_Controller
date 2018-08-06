8502e2ffc322213ca1bd719b02fd934a078d0285
Cheitovilla <cheitovilla@outlook.com>
2018-08-06 17:43:21 -0500

    Done!

---
+++ New Unity Project/Assets/Scripts/Matriz.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Matriz : MonoBehaviour {
+    public bool star;
+    public int rangocolor;
+    public int rangoMatriz;
+    Color colorR = Color.red;
+    Color colorG = Color.green;
+    Color colorB = Color.blue;
+    Color colorY = Color.yellow;
+    public bool play;
+    
+    
+
+    // Use this for initialization
+    void Start(){
+        
+    }
+
+    void IniciarGame()
+    {
+        ArrayList list = new ArrayList();
+        rangoMatriz = Random.Range(3, 13);
+        for (int i = 0; i < rangoMatriz; i++)
+        {
+            for (int j = 0; j < rangoMatriz; j++)
+            {
+                GameObject bolas = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                bolas.transform.position = new Vector3(i * 2.0f, j * 2.0f, 0.0f);
+                list.Add(bolas);
+                rangocolor = Random.Range(0, 4);
+                if (rangocolor == 0)
+                {
+                    bolas.GetComponent<Renderer>().material.color = colorR;
+                }
+                else if (rangocolor == 1)
+                {
+                    bolas.GetComponent<Renderer>().material.color = colorG;
+                }
+                else if (rangocolor == 2)
+                {
+                    bolas.GetComponent<Renderer>().material.color = colorB;
+                }
+                else if (rangocolor == 3)
+                {
+                    bolas.GetComponent<Renderer>().material.color = colorY;
+                }
+               
+               
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (star)
+        {
+          //  Destroy(gameObject);
+            IniciarGame();
+            star = false;
+        }
+        if (play)
+        {
+
+        }
+        
+    }
+
+           
+
+
+
+}
---
+++ New Unity Project/Assets/Scripts/dupli.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dupli : MonoBehaviour {
+	
+	public GameObject player;
+	public Transform position;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.y < -50) {
+			player.transform.position = position.transform.position;
+		}		
+}
+}
